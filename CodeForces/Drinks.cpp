4ea64ff81b7b52ba6929f1311fc862dd64dee832
Falguni Agrawal <falguniagrawal1703@gmail.com>
2026-03-24 19:26:11 +0530

    Create Drinks.cpp

--- CodeForces/Drinks.cpp
+++ CodeForces/Drinks.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n;
     cin>>n;
     double sum;
     double p;
-    for(int i=0;i<n;i++){ // store value without using array so that we can trace it
+    for(int i=0;i<n;i++)
+    {                           // store value without using array so that we can trace it
         cin>>p;
         sum+=p;
     }
